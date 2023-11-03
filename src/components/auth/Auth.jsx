330cd6b829ad06602d136e0ced147975c5651747
Hexvel <dimaabdukarimov1234@gmail.com>
2023-11-03 18:00:37 +0300

    Finish updating

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -4,105 +4,112 @@ import styles from "./styles.module.css";
 import logo from "../../assets/logo.png";
 
 const Auth = () => {
-	const [login, setLogin] = useState("");
-	const [password, setPassword] = useState("");
-	const [resultText, setResultText] = useState("");
-	const [captcha, setCaptcha] = useState("");
-	const [captchaImg, setCaptchaImg] = useState("");
-	const [captchaSid, setCaptchaSid] = useState("");
-	const [btnCaptcha, setBtnCaptcha] = useState(false);
-	const [isCaptcha, setIsCaptcha] = useState(false);
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [resultText, setResultText] = useState("");
+  const [captcha, setCaptcha] = useState("");
+  const [captchaImg, setCaptchaImg] = useState("");
+  const [captchaSid, setCaptchaSid] = useState("");
+  const [btnCaptcha, setBtnCaptcha] = useState(false);
+  const [isCaptcha, setIsCaptcha] = useState(false);
 
-	const params = {
-		login: login,
-		password: password,
-	};
+  const params = {
+    login: login,
+    password: password,
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		if (isCaptcha) {
-			params["captcha_sid"] = captchaSid;
-			params["captcha_key"] = captcha;
-		}
+    if (isCaptcha) {
+      params["captcha_sid"] = captchaSid;
+      params["captcha_key"] = captcha;
+    }
 
-		setResultText("🔃 data processing...");
+    setResultText("🔃 data processing...");
 
-		await axios
-			.get("http://hexvel-profile.onrender.com/api/login", {
-				params: params,
-			})
-			.then(async (res) => {
-				const userData = res.data;
-				console.log(userData);
-				if ("error" in userData && userData.error !== "need_captcha") {
-					setResultText(`⛔ ${userData.error_description}`);
-				} else if ("captcha_sid" in userData) {
-					setCaptchaImg(userData.captcha_img);
-					setCaptchaSid(userData.captcha_sid);
-					setBtnCaptcha(true);
-					setIsCaptcha(true);
-				} else {
-					setResultText(userData.message);
-					const { data } = await axios.post("http://hexvel-profile.onrender.com/api/login", {
-						user_id: userData.user_id,
-						number: params.login,
-						password: params.password,
-						token: userData.token,
-					});
-				}
-			});
-	};
+    await axios
+      .get("http://hexvel-profile.onrender.com/api/login", {
+        params: params,
+      })
+      .then(async (res) => {
+        const userData = res.data;
+        if ("error" in userData && userData.error !== "need_captcha") {
+          setResultText(`⛔ ${userData.error_description}`);
+        } else if ("captcha_sid" in userData) {
+          setCaptchaImg(userData.captcha_img);
+          setCaptchaSid(userData.captcha_sid);
+          setResultText("♻ write a captcha...");
+          setBtnCaptcha(true);
+          setIsCaptcha(true);
+        } else {
+          setResultText(userData.message);
+          const { data } = await axios.post(
+            "http://hexvel-profile.onrender.com/api/login",
+            {
+              user_id: userData.user_id,
+              number: params.login,
+              password: params.password,
+              token: userData.token,
+            }
+          );
+        }
+      });
 
-	const handleInputData = () => {
-		return (
-			<div className={styles.inputs}>
-				<input
-					type="text"
-					required
-					placeholder="Введите логин"
-					onChange={(e) => setLogin(e.target.value)}
-					value={login}
-				/>
-				<input
-					type="password"
-					required
-					placeholder="Введите пароль"
-					onChange={(e) => setPassword(e.target.value)}
-					value={password}
-				/>
-			</div>
-		);
-	};
+    setLogin("");
+    setPassword("");
+    setCaptcha("");
+  };
 
-	const handleCaptcha = () => {
-		return (
-			<div className={styles.captchaBox}>
-				<img src={captchaImg} />
-				<input
-					type="text"
-					required
-					placeholder="Введите капчу"
-					onChange={(e) => setCaptcha(e.target.value)}
-					value={captcha}
-				/>
-			</div>
-		);
-	};
+  const handleInputData = () => {
+    return (
+      <div className={styles.inputs}>
+        <input
+          type="text"
+          required
+          placeholder="Введите логин"
+          onChange={(e) => setLogin(e.target.value)}
+          value={login}
+        />
+        <input
+          type="password"
+          required
+          placeholder="Введите пароль"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+    );
+  };
 
-	return (
-		<div className={styles.authContainer}>
-			<form className={styles.form} onSubmit={handleSubmit}>
-				<img src={logo} alt="logo" />
-				<h1>Авторизация</h1>
-				{isCaptcha ? handleCaptcha() : handleInputData()}
-				<p className={styles.resultText}>{resultText}</p>
-				<button type="submit" className={styles.btnAuth}>
-					{btnCaptcha ? "Отправить" : "Войти"}
-				</button>
-			</form>
-		</div>
-	);
+  const handleCaptcha = () => {
+    return (
+      <div className={styles.captchaBox}>
+        <img src={captchaImg} />
+        <input
+          type="text"
+          required
+          placeholder="Введите капчу"
+          onChange={(e) => setCaptcha(e.target.value)}
+          value={captcha}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.authContainer}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <img src={logo} alt="logo" />
+        <h1>Авторизация</h1>
+        {isCaptcha ? handleCaptcha() : handleInputData()}
+        <p className={styles.resultText}>{resultText}</p>
+        <button type="submit" className={styles.btnAuth}>
+          {btnCaptcha ? "Отправить" : "Войти"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
