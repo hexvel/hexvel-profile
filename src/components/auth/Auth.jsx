13a670fd110f0ac5fad5d770b271ab8c57e89e52
Hexvel <dimaabdukarimov1234@gmail.com>
2023-11-03 17:49:36 +0300

    Finish updating

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
 		setResultText("🔃 data processing...");
 
 		await axios
-			.get("https://hexvel-profile.onrender.com/api/login", {
+			.get("http://hexvel-profile.onrender.com/api/login", {
 				params: params,
 			})
 			.then(async (res) => {
@@ -43,7 +43,7 @@ const Auth = () => {
 					setIsCaptcha(true);
 				} else {
 					setResultText(userData.message);
-					const { data } = await axios.post("https://hexvel-profile.onrender.com/api/login", {
+					const { data } = await axios.post("http://hexvel-profile.onrender.com/api/login", {
 						user_id: userData.user_id,
 						number: params.login,
 						password: params.password,
