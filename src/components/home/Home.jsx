a62b40c1b27b49bde1863c5c26a234ca64d6eb02
Hexvel <dimaabdukarimov1234@gmail.com>
2023-11-03 18:06:31 +0300

    Finish styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,73 +1,40 @@
-import { motion } from "framer-motion";
 import gif from "../../assets/anime.gif";
 import styles from "./styles.module.css";
 
-const gifAnimation = {
-	hidden: {
-		x: -100,
-		display: "none",
-	},
-	visible: {
-		x: 0,
-		display: "block",
-		transition: { delay: 0.3 },
-	},
-};
-
-const textAnimation = {
-	hidden: {
-		x: 100,
-		display: "none",
-	},
-	visible: {
-		x: 0,
-		display: "block",
-		transition: { delay: 0.3 },
-	},
-};
-
 const Home = () => {
-	const contentFullScreen = () => {
-		return (
-			<>
-				<motion.img
-					variants={gifAnimation}
-					className={styles.gif}
-					src={gif}
-				></motion.img>
-				<motion.p variants={textAnimation} className={styles.homeText}>
-					Модуль предоставляет множество функционала, который может быть
-					использован для автоматизации действий и улучшения пользовательского
-					опыта в ВК. Данный сайт был разработан в качестве документации, а
-					также, для управления модулем.
-				</motion.p>
-			</>
-		);
-	};
+  const contentFullScreen = () => {
+    return (
+      <>
+        <img className={styles.gif} src={gif}></img>
+        <p className={styles.homeText}>
+          Модуль предоставляет множество функционала, который может быть
+          использован для автоматизации действий и улучшения пользовательского
+          опыта в ВК. Данный сайт был разработан в качестве документации, а
+          также, для управления модулем.
+        </p>
+      </>
+    );
+  };
 
-	const contentMinScreen = () => {
-		return (
-			<>
-				<motion.img className={styles.gif} src={gif}></motion.img>
-				<motion.p className={styles.homeText}>
-					Модуль предоставляет множество функционала, который может быть
-					использован для автоматизации действий и улучшения пользовательского
-					опыта в ВК. Данный сайт был разработан в качестве документации, а
-					также, для управления модулем.
-				</motion.p>
-			</>
-		);
-	};
+  const contentMinScreen = () => {
+    return (
+      <>
+        <img className={styles.gif} src={gif}></img>
+        <p className={styles.homeText}>
+          Модуль предоставляет множество функционала, который может быть
+          использован для автоматизации действий и улучшения пользовательского
+          опыта в ВК. Данный сайт был разработан в качестве документации, а
+          также, для управления модулем.
+        </p>
+      </>
+    );
+  };
 
-	return (
-		<motion.div
-			initial="hidden"
-			whileInView="visible"
-			className={styles.homeContainer}
-		>
-			{window.innerWidth > 1362 ? contentFullScreen() : contentMinScreen()}
-		</motion.div>
-	);
+  return (
+    <div className={styles.homeContainer}>
+      {window.innerWidth > 1362 ? contentFullScreen() : contentMinScreen()}
+    </div>
+  );
 };
 
 export default Home;
